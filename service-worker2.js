14a4e693b3ed4f1df559895fb273cdb0c5f39862
Albert <alberto.crespellperez@telefonica.com>
2015-03-23 13:03:10 +0100

    move from dump to console.log

--- service-worker2.js
+++ service-worker2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function debug(str) {
-  dump(' -*- ServiceWorkers - Worker2 -*-: ' + str + '\n');
+  console.log(' -*- ServiceWorkers - Worker2 -*-: ' + str + '\n');
 }
 
 self.addEventListener('install', function(e) {
