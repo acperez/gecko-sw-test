30b4f446f110bad3569e638078d1cd7a59243013
Albert <alberto.crespellperez@telefonica.com>
2015-04-23 14:45:56 +0200

    new ver

--- service-worker.js
+++ service-worker.js
@@ -102,7 +102,7 @@ function testClient() {
 }
 
 self.onmessage = function(e) {
-  debug('Message received 2 testing : ' + e.data);
+  debug('Message received 3 testing : ' + e.data);
   switch (e.data) {
     case 'openWindow':
       testOpenWindow();
