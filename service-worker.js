9197f6616c392c9112b119969ab9b1b481f6e019
Albert <alberto.crespellperez@telefonica.com>
2015-04-30 11:15:08 +0200

    update

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ function debug(str) {
 
 self.addEventListener('install', function(e) {
   debug('Install event');
-  debug('Install version 6');
+  debug('Install version 7');
 });
 
 self.addEventListener('activate', function(e) {
@@ -77,7 +77,7 @@ function testClient() {
 }
 
 self.onmessage = function(e) {
-  debug('Message received 6: ' + e.data);
+  debug('Message received 7: ' + e.data);
   switch (e.data) {
     case 'openWindow':
       testOpenWindow();
